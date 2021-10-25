c34e5bd32e5105b26b861934ee2a6cc6952a6a75
Merrill Shen <50651325+merrillshen@users.noreply.github.com>
2021-10-25 12:08:27 +0100

    Exercise 1 Task 2 Completion: Complete the interrupt initialisation function and ISR to toggle the LED on RH3 when the LDR goes from light to dark.

--- interrupts.c
+++ interrupts.c
@@ -9,6 +9,9 @@ void Interrupts_init(void)
 {
 	// turn on global interrupts, peripheral interrupts and the interrupt source 
 	// It's a good idea to turn on global interrupts last, once all other interrupt configuration is done.
+    INTCONbits.PEIE=1; 	//enable peripheral interrupts
+    PIE2bits.C1IE=1;    //enable Comparator 1 interrupt bit (interrupt source)
+    INTCONbits.GIE=1; 	//enable interrupts globally 
 }
 
 /************************************
@@ -16,7 +19,10 @@ void Interrupts_init(void)
  * Make sure all enabled interrupts are checked and flags cleared
 ************************************/
 void __interrupt(high_priority) HighISR()
-{
-	//add your ISR code here i.e. check the flag, do something (i.e. toggle an LED), clear the flag...
+{   //add your ISR code here i.e. check the flag, do something (i.e. toggle an LED), clear the flag...
+    if (PIR2bits.C1IF == 1){ 
+        LATHbits.LATH3 = !LATHbits.LATH3; //toggle LED
+        PIR2bits.C1IF=0; //toggle off flag
+    }
 }
 
