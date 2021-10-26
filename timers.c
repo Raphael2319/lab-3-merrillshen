34cbe921b10628c1fa39209d96abe02d023aefb8
Merrill Shen <50651325+merrillshen@users.noreply.github.com>
2021-10-26 11:19:05 +0100

    Exercise 2 Task 3 Completion:Adjust the prescaler so that the timer overflows approximately every second.

--- timers.c
+++ timers.c
@@ -8,7 +8,8 @@ void Timer0_init(void)
 {
     T0CON1bits.T0CS=0b010; // Fosc/4
     T0CON1bits.T0ASYNC=1; // see datasheet errata - needed to ensure correct operation when Fosc/4 used as clock source
-    T0CON1bits.T0CKPS=0b1111; // 1:32768
+    //T0CON1bits.T0CKPS=0b1111; // 1:32768
+    T0CON1bits.T0CKPS=0b1000; // 1:256, Value: 244.1443
     T0CON0bits.T016BIT=1;	// 16 bit mode	
 	
     // it's a good idea to initialise the timer registers so we know we are at 0
