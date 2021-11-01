2fb9b4b461c366365a9866380485163ef216c482
Merrill Shen <50651325+merrillshen@users.noreply.github.com>
2021-11-01 23:13:12 +0000

    Exercise 2 Task 5 Completion: Update to TMR0L write

--- timers.c
+++ timers.c
@@ -8,13 +8,12 @@ void Timer0_init(void)
 {
     T0CON1bits.T0CS=0b010; // Fosc/4
     T0CON1bits.T0ASYNC=1; // see datasheet errata - needed to ensure correct operation when Fosc/4 used as clock source
-    //T0CON1bits.T0CKPS=0b1111; // 1:32768
-    T0CON1bits.T0CKPS=0b1000; // 1:256, Value: 244.1443
+    T0CON1bits.T0CKPS=0b1000; // 1:256
     T0CON0bits.T016BIT=1;	// 16 bit mode	
 	
     // it's a good idea to initialise the timer registers so we know we are at 0
-    TMR0H=62500;  //write High reg first, update happens when low reg is written to
-    TMR0L=0;
+    TMR0H=65535;  //write High reg first, update happens when low reg is written to
+    TMR0L=3035; // number of intervals needed 62500
     T0CON0bits.T0EN=1;	//start the timer
 }
 
